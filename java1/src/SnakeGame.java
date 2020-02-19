1ee105a15bc87bf502c3a6d34253aa0d2969d413
alan <agamez8@miners.utep.edu>
2020-02-19 14:46:44 -0700

    Finished cosntructors for snakegame.java

---
+++ java1/src/SnakeGame.java
@@ -0,0 +1,58 @@
+public class SnakeGame {
+
+    //ELEMENTS
+    private boolean[][] game(){
+
+    }
+
+    private int[] headPosition(){
+
+    }
+
+    private static int exhaustiveChecks(){
+
+    }
+
+    private static int recursiveChecks(){
+
+    }
+
+    //DEFAULT CONSTRUCTORS
+    public gameBoard(){
+        int[] grid;
+        return grid;
+    }
+
+    public snakeHead(boolean[][] head, int x, int y){
+
+        return head;
+    }
+
+    //METHODS
+    public int[] findTailExhaustive(int x, int y){
+
+    }
+
+    public int[] findTailRecursive(int x, int y){
+
+    }
+
+    private int[] findTailRecursive(int[] currentPosition, int[] previousPosition){
+
+    }
+
+    private void resetCounters(){
+        //recursiveCheck = 0
+        //exhaustiveCheck = 0
+    }
+
+    //CURRENT CHECKS
+    private static int getRecursiveChecks(){
+        
+    }
+
+    private static int getExhaustiveChecks(){
+
+    }
+
+}
